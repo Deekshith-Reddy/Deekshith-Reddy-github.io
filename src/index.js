670de6446b5add7e0ae66fadc2ebd00cc79fc20a
deekshith <deekshithreddy1317@gmail.com>
2020-12-10 18:43:38 +0530

    Updates

---
+++ src/index.js
@@ -0,0 +1,47 @@
+var canvas = document.getElementById("tutorial");
+var ctx = canvas.getContext('2d');
+const { lerp } = require('canvas-sketch-util/math')
+var random = require('canvas-sketch-util/random');
+var palettes = require('nice-color-palettes');
+
+const palette = random.shuffle(random.pick(palettes)).slice(1, 6);
+
+const creatGrid = () => {
+    const points = [];
+    const count = 20;
+    for (let i = 0; i < count; i++) {
+        for (let j = 0; j < count; j++) {
+            const u = count <= 1 ? 0.5 : i / (count - 1);
+            const v = count <= 1 ? 0.5 : j / (count - 1);
+            const radius = Math.abs(random.noise2D(u, v)) * 0.005 * count;
+            points.push({
+                color: random.pick(palette),
+                radius,
+                position: [u, v]
+            });
+        }
+
+    }
+    return points;
+};
+
+const points = creatGrid();
+const margin = 100;
+const width = canvas.offsetWidth;
+const height = canvas.offsetHeight;
+
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, width, height);
+
+points.forEach(data => {
+    const { color, position, radius } = data;
+    const [u, v] = position;
+    const x = lerp(margin, width - margin, u);
+    const y = lerp(margin, height - margin, v);
+
+
+    ctx.beginPath();
+    ctx.arc(x, y, width * radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = color;
+    ctx.fill();
+})
